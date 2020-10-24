d41eedf07361f638db6dd67fc6a5342e5c805fff
alex.zhong <alex.zhong@sneakertrader.com>
2020-10-24 15:44:02 +1300

    Update main package.json commands and rename build script

--- ecr_image_build_deploy.sh
+++ ecr_image_build_deploy.sh
@@ -15,7 +15,7 @@ sudo docker build -t "${LOCAL_IMAGE_NAME}:${TAG_NAME}" .
 sudo docker tag "${LOCAL_IMAGE_NAME}:${TAG_NAME}" "${ECR_URL}:${TAG_NAME}"
 sudo docker push "${ECR_URL}:${TAG_NAME}"
 
-SERVICE_NAME="express-alb-service-prod"
+SERVICE_NAME="st-blue-green-service"
 
 # use this command if the service using rolling updates
 aws ecs update-service --cluster sneaker-trader-cluster --service $SERVICE_NAME --force-new-deployment
