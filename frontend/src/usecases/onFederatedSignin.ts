1d8f671c35a375e42c6ad136debda3d151a5c957
alex.zhong <alex.zhong@sneakertrader.com>
2020-11-19 14:00:43 +1300

    Add social provider username as default db username on signup

--- frontend/src/usecases/onFederatedSignin.ts
+++ frontend/src/usecases/onFederatedSignin.ts
@@ -5,18 +5,17 @@ import { UserController } from 'api/controllers/UserController';
 import { SocialProvider } from '../../../shared';
 import { UserRegistrationController } from 'api/controllers/UserRegistrationController';
 
-const onFederatedSignin = (UserInstance: UserController, UserRegistrationInstance: UserRegistrationController) => async (
-  provider: SocialProvider,
-  token: string,
-  expires_at: number,
-  user: { email: string; name: string }
-) => {
+const onFederatedSignin = (
+  UserInstance: UserController,
+  UserRegistrationInstance: UserRegistrationController
+) => async (provider: SocialProvider, token: string, expires_at: number, user: { email: string; name: string }) => {
   // if the user has used the email already but not using the current provider, reject the user from signin
 
   const currUser = await UserInstance.getByEmail(user.email);
   if (currUser && currUser.signinMethod !== provider) throw new Error('Email already exists');
   // otherwise create the user if it does not exist
-  if (!currUser) await UserRegistrationInstance.register({ email: user.email, username: user.name, signinMethod: provider });
+  if (!currUser)
+    await UserRegistrationInstance.register({ email: user.email, username: user.name, signinMethod: provider });
 
   await Auth.federatedSignIn(provider as string, { token, expires_at }, user);
 };
