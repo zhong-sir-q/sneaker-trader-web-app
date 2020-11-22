b68dba5fe7a525d31683dbfddb1b7cea6b466235
alex <0alexzhong0@gmail.com>
2020-11-23 09:56:23 +1300

    Redirect to consent screen and fetch tokens

--- backend/src/api/routers/auth.ts
+++ backend/src/api/routers/auth.ts
@@ -1,59 +1,50 @@
 // auth handles 3rd-party authentication services such as Google and Facebook
 import { Router, Request, Response, NextFunction } from 'express';
 
-import { google } from 'googleapis';
+import queryString from 'query-string';
+import fetch from 'node-fetch';
 
 function createAuthHandlers() {
   // run gcloud auth application-default login to setup default google credentials
 
   // more info about google api client on https://googleapis.dev/nodejs/googleapis/latest/oauth2/index.html#oauth2-client
-  const auth = new google.auth.GoogleAuth();
-
-  const askConsent = async (_: Request, res: Response, next: NextFunction) => {
-    try {
-      const authClient = await auth.getClient();
-
-      const consentUrl = authClient.generateAuthUrl({
-        redirect_uri: 'https://localhost:3000',
-        scope: [
-          'openid',
-          'https://www.googleapis.com/auth/userinfo.email',
-          'https://www.googleapis.com/auth/userinfo.profile',
-        ],
-        access_type: 'offline',
-      });
-
-      res.json(consentUrl);
-    } catch (e) {
-      next(e);
-    }
-  };
+  const client_id = '1021202892438-u553lbg9ns1k2fkluhhscloutqlmn2o7.apps.googleusercontent.com';
+  const client_secret = 'GObZWr515UuN2xO_-XPdDx_t';
+  const redirect_uri = 'https://localhost:3000/auth/signin';
+  const grant_type = 'authorization_code';
 
   // returns the Credentials object that contains the access_token, id_token and expiry_date etc.
-  const exchangeCodeForToken = async (req: Request, res: Response, next: NextFunction) => {
+  const exchangeCodeForTokens = async (req: Request, res: Response, next: NextFunction) => {
     const { code } = req.body;
 
-    try {
-      const authClient = await auth.getClient();
+    let authTokensUrl = 'https://oauth2.googleapis.com/token';
 
-      const { tokens } = await authClient.getToken(code);
+    const params = queryString.stringify({ code, client_id, client_secret, grant_type, redirect_uri });
+    authTokensUrl = authTokensUrl + '?' + params;
+
+    try {
+      const tokens = await fetch(authTokensUrl, {
+        method: 'POST',
+      }).then(r => r.json());
+
+      console.log(authTokensUrl)
+      console.log(tokens)
+
       res.json(tokens);
-    } catch (e) {
-      next(e);
+    } catch (err) {
+      next(err);
     }
   };
 
-  return { askConsent, exchangeCodeForToken };
+  return { exchangeCodeForTokens };
 }
 
 export default function createAuthRoutes() {
   const auth = Router();
 
-  const { askConsent, exchangeCodeForToken } = createAuthHandlers();
+  const { exchangeCodeForTokens } = createAuthHandlers();
 
-  auth.route('/google/consent').get(askConsent);
-
-  auth.route('/google/tokens').post(exchangeCodeForToken);
+  auth.route('/google/tokens').post(exchangeCodeForTokens);
 
   return auth;
 }
--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,9 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form as FormikForm, Formik } from 'formik';
-import { Link } from 'react-router-dom';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 
 // reactstrap components
 import { Card, CardBody, CardHeader, CardFooter, Container, Col, Button } from 'reactstrap';
+import styled from 'styled-components';
+
+import queryString from 'query-string';
+import jwt_decode from "jwt-decode"
 import * as Yup from 'yup';
 
 // components
@@ -19,6 +23,8 @@ import bgImage from 'assets/img/bg14.jpg';
 import { signIn } from 'utils/auth';
 import SignInWithGoogle from 'components/buttons/SigninWithGoogle';
 import SignInWithFacebook from 'components/buttons/SignInWithFacebook';
+import formatApiEndpoint from 'utils/formatApiEndpoint';
+import formatRequestOptions from 'utils/formatRequestOptions';
 
 type SignInFormStateType = {
   email: string;
@@ -35,10 +41,92 @@ const validationSchema = Yup.object({
   password: minCharacters(8),
 });
 
+type GoogleSinginButtonProps = {
+  onClick: () => void;
+  img: string;
+  btnText: string;
+};
+
+const StyledSigninButton = styled.button`
+  background-color: white;
+  color: black;
+  width: 100%;
+  font-size: 1em;
+  padding: 10px 0;
+  margin-bottom: 15px;
+`;
+
+const StyledSigninButtonImg = styled.img`
+  margin-bottom: 3px;
+  margin-right: 5px;
+  width: 20px;
+`;
+
+const SigninButton = (props: GoogleSinginButtonProps) => {
+  return (
+    <StyledSigninButton onClick={props.onClick} type='button'>
+      <StyledSigninButtonImg alt='Signin button image' src={props.img} />
+      {props.btnText}
+    </StyledSigninButton>
+  );
+};
+
+const googleOAuth2Url = () => {
+  const oauthConsentUrl = 'https://accounts.google.com/o/oauth2/auth';
+  const response_type = 'code';
+  const client_id = '1021202892438-g7803i6ust97vvb8mojt8lneu1fdajgd.apps.googleusercontent.com';
+  const scope = 'openid email profile';
+  const redirect_uri = 'https://localhost:3000/auth/signin';
+
+  const queries = queryString.stringify({ response_type, client_id, scope, redirect_uri });
+
+  return oauthConsentUrl + '?' + queries;
+};
+
+// sign in with google by redirect
+const GoogleSigninButton = () => {
+  // TODO: store and use the image locally
+  const btnImg =
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png';
+
+  return (
+    <SigninButton
+      img={btnImg}
+      btnText='Signin With Google'
+      onClick={() => (window.location.href = googleOAuth2Url())}
+    />
+  );
+};
+
 const SignIn = () => {
   const [loginError, setLoginError] = useState<string>();
 
-  // TODO: handle same username error after social signin
+  const location = useLocation();
+
+  const exchangeCodeForTokens = (code: string) => fetch(formatApiEndpoint('auth/google/tokens'), formatRequestOptions({ code })).then(res => res.json())
+
+  useEffect(() => {
+    const { search } = location;
+
+    const urlParams = new URLSearchParams(search)
+    const googleAuthCode = urlParams.get('code')
+    
+    // the user has signed in successfully with google
+    if (googleAuthCode) {
+      // retrive the info about the user and signin with AWS
+      (async () => {
+        console.log('I am here')
+        // user may input random auth code into the url and that will invalidate the request
+        const tokens = await exchangeCodeForTokens(googleAuthCode)
+
+        console.log(tokens)
+
+        // const googleUser = jwt_decode(id_token)
+        // console.log(googleAuthCode)
+      })()
+    }
+  }, [location]);
+
   const updateLoginErr = (message: string) => setLoginError(message);
 
   const onSignin = async (email: string, pw: string) => {
@@ -70,7 +158,8 @@ const SignIn = () => {
                           <img src={stLogo} alt='sneakertrader-logo' />
                         </Link>
                       </div>
-                      <SignInWithGoogle handleSignin={updateLoginErr} />
+                      <GoogleSigninButton />
+                      {/* <SignInWithGoogle handleSignin={updateLoginErr} /> */}
                       <SignInWithFacebook handleSignin={updateLoginErr} />
                     </CardHeader>
                     <CardBody>
@@ -94,7 +183,14 @@ const SignIn = () => {
                       />
                     </CardBody>
                     <CardFooter>
-                      <Button block type='submit' color='primary' size='lg' className='mb-3 btn-round' data-testid='signin-btn'>
+                      <Button
+                        block
+                        type='submit'
+                        color='primary'
+                        size='lg'
+                        className='mb-3 btn-round'
+                        data-testid='signin-btn'
+                      >
                         Login
                       </Button>
                       <div className='pull-left'>
