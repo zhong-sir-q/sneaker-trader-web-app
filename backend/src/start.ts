4791de96cf1038341966cf2f8f7a275c6b5d49fc
alex <0alexzhong0@gmail.com>
2020-12-09 20:48:27 +1300

    Remove unnecessary log statement in backend/src/start.ts

--- backend/src/start.ts
+++ backend/src/start.ts
@@ -1,36 +1,29 @@
 import app from './app';
 import ChatService from './services/ChatService';
+
 const PORT = process.env.PORT || 4000;
-var http = require('http').Server(app);
+const http = require('http').Server(app);
+
 const io = require('socket.io')(http, {
-    cors: {
-      origin: '*',
-    }
-  });
+  cors: {
+    origin: '*',
+  },
+});
 
 const ChatServiceInstance = new ChatService();
 
-io.on('connection', function(socket: any) {
-    console.log('A user connected');
-    socket.on("join", async function(room: any) {
-      socket.join(room);
-      io.emit("roomJoined", room);
-    });
-    socket.on("message", async function(data: any) {
-        const { message, productId, buyerId, sellerId, userType} = data;
-        io.emit("newMessage", message);
-    ChatServiceInstance.sendMessage(Number(productId), Number(buyerId), Number(sellerId), message, userType)
-      .then(() => console.log('Sent message'))
-      .catch(() => {console.log('llll')});
+io.on('connection', function (socket: any) {
+  socket.on('message', async function (data: any) {
+    const { message, productId, buyerId, sellerId, userType } = data;
 
-    //Whenever someone disconnects this piece of code executed
-    socket.on('disconnect', function () {
-       console.log('A user disconnected');
-    });
- });
- 
+    io.emit('newMessage', message);
+
+    ChatServiceInstance.sendMessage(Number(productId), Number(buyerId), Number(sellerId), message, userType);
+
+    socket.on('disconnect', () => {});
+  });
 });
- 
-http.listen(PORT, function() {
+
+http.listen(PORT, function () {
   console.log('listening on *:' + PORT);
 });
